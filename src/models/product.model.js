dff571f844aab0fa0bca5b9d1308e456da11451b
Manjunath B V <manjubvmtr122333@gmail.com>
2022-08-02 16:56:35 +0530

    make it better

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
+    name: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String, required: true },
     count: { type: Number, required: true },
